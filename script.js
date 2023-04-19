663501836cfbd335e8422a068d58fbbed939f2be
Jatin Hinduja <jatinhinduja4@gmail.com>
2023-04-19 22:05:44 +0530

    second commit

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const btnNew=document.querySelector('.btn--new');
 const btnRoll=document.querySelector('.btn--roll');
 const btnHold=document.querySelector('.btn--hold');
 
+//ajit singh
 score0El.textContent=0;
 score1El.textContent=0;
 diceEl.classList.add('hidden');
